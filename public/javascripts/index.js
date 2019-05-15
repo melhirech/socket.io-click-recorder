c6fcdd261f6eea2a4df531ba268bf18ad85b6231
Elhirech Mohamed <mhm.elhirech@gmail.com>
2019-05-15 11:42:50 +0000

    initial commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,32 @@
+const socket = io()
+
+const click = document.getElementById("click")
+let timer = null
+
+document.onclick = e => {
+  socket.emit("click", { x: event.clientX, y: event.clientY, timer: timer });
+};
+
+document.onmousedown = e => {
+    timer = new Date()
+};
+
+document.onmouseup = e => {
+    timer = new Date - timer
+};
+
+socket.on("remoteClick", data => {
+  console.log(data);
+  click.classList.remove("hidden");
+  click.style.left = data.x + "px";
+  click.style.top = data.y + "px";
+  click.innerText = data.timer/1000
+
+  setTimeout(() => {
+    addClass();
+  }, 3000);
+});
+
+const addClass = () => {
+  click.classList.add("hidden");
+};
